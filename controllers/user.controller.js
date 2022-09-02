995a89073c1504efe900dbb9690ae881c80d3209
zhSHUVO <zhshuvo99@gmail.com>
2022-09-02 11:54:34 +0600

    getting all user data

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,13 @@
+const fs = require('fs');
+
 module.exports.getRandomUser = (req, res) => {
     res.send('random user');
 };
 
 module.exports.getAllUser = (req, res) => {
-    res.send('all users');
+    const userdata = fs.readFileSync('random.json');
+    const user = JSON.parse(userdata);
+    res.send(user);
 };
 
 module.exports.addRandomUser = (req, res) => {
